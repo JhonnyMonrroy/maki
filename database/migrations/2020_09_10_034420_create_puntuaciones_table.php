ddce6532f45c5646dfe8b20e82055b77cfe16160
Jhonny Monrroy <jhonnymonrroy@yahoo.es>
2020-09-10 00:05:48 -0400

    Modelando base de datos inicial

---
+++ database/migrations/2020_09_10_034420_create_puntuaciones_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePuntuacionesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('puntuaciones', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('trabajo_id')->references('id')->on('trabajos')->onDelete('cascade');
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('puntuaciones');
+    }
+}
